a8c96ff3f55b05f3151de23f5c70e198274be272
Your Name <you@example.com>
2022-07-03 20:39:11 +0430

    Themes Recognized Windows Style Changed MyStyle Started

--- MessagingApplication/MainWindow.xaml.cs
+++ MessagingApplication/MainWindow.xaml.cs
@@ -19,10 +19,11 @@ namespace MessagingApplication
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MyStyleWindow
     {
         MessageSender msgSender = new MessageSender();
         MessageListener msgListener;
+
         public MainWindow()
         {
             InitializeComponent();
---
+++ MessagingApplication/Themes/MyStyle.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MessagingApplication
+{
+    partial class MyStyle : ResourceDictionary
+    {
+
+        Window window;
+
+        public MyStyle()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            window.DragMove();
+        }
+
+
+        private void Close_Clicked(object sender, RoutedEventArgs e)
+        {
+            window.Close();
+            if (App.Current.MainWindow == window)
+                Environment.Exit(0);
+        }
+
+        private void Maxmize_Clicked(object sender, RoutedEventArgs e)
+        {
+            window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void Minimize_Button(object sender, RoutedEventArgs e)
+        {
+            window.WindowState = WindowState.Minimized;
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            window = sender as Window;
+        }
+
+    }
+
+    public class MyStyleWindow : Window
+    {
+        public object TitleBarContent
+        {
+            get { return (object)GetValue(TitleBarContentProperty); }
+            set { SetValue(TitleBarContentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TitleBarContent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleBarContentProperty =
+            DependencyProperty.Register("TitleBarContent", typeof(object), typeof(MyStyleWindow), new PropertyMetadata("Title Bar Content"));
+
+
+
+
+
+        public double TitleBarHeight
+        {
+            get { return (double)GetValue(TitleBarHeightProperty); }
+            set { SetValue(TitleBarHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TitleBarHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleBarHeightProperty =
+            DependencyProperty.Register("TitleBarHeight", typeof(double), typeof(MyStyleWindow), new PropertyMetadata(30.0));
+
+
+
+    }
+}
