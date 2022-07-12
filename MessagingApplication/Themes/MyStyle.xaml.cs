0fa1f54054f743ff7cc4b8172c89bfe65eb5556b
Your Name <you@example.com>
2022-07-12 20:24:15 +0430

    Textbox Styled Mainlayout updated message input textbox added

--- MessagingApplication/Themes/MyStyle.xaml.cs
+++ MessagingApplication/Themes/MyStyle.xaml.cs
@@ -78,4 +78,20 @@ namespace MessagingApplication
 
 
     }
+
+    public class MyStyleTextboxWithPlaceHolder : TextBox
+    {
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Placeholder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register("Placeholder", typeof(string), typeof(MyStyleTextboxWithPlaceHolder), new PropertyMetadata(""));    
+
+
+    }
+
 }
